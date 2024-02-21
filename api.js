f743bd01aa3ba6274def7dff0bd425e578fb568d
System Administrator <root@MacBook-Pro-Yan.local>
2024-02-22 01:25:14 +0200

    add form

--- api.js
+++ api.js
@@ -1,6 +1,19 @@
+const userData = {
+    username : document.getElementById("username").value,
+    mail : document.getElementById("mail").value,
+    password : document.getElementById("password").value,
+    country : document.getElementById("country").value,
+}
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", ev => {
 
 
 
+
+
+})
 
 
 
